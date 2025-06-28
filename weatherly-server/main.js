a9008c8bece09f0b7cf3a66af9b83db8b404ea4c
AMRITESH-MISHRA-15 <amriteshmishra2004@gmail.com>
2025-06-28 22:59:45 +0530

    Implemented & Tested User Registration & Login API

--- weatherly-server/main.js
+++ weatherly-server/main.js
@@ -2,12 +2,15 @@ const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 // Default route
 app.get('/', (req, res) => {
